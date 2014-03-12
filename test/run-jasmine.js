da0c8dd0be206c0e0809e624dbdf01fa3c604c61
treelite <c.xinle@gmail.com>
2014-03-12 13:53:11 +0800

    mark normal console.log

--- test/run-jasmine.js
+++ test/run-jasmine.js
@@ -12,28 +12,34 @@ var rc;
 
 var page = require('webpage').create();
 
-// Route "console.log()" calls from within the Page context
-// to the main Phantom context (i.e. current "this")
+// Route 'console.log()' calls from within the Page context
+// to the main Phantom context (i.e. current 'this')
 
-page.onConsoleMessage = function (msg, tag) {
-    system.stdout.write(msg);
-    var match = doneRegEx.exec(msg);
-    if (match) {
-        rc = match[1] === "0" ? 0 : 1;
-        return;
+page.onConsoleMessage = function (msg) {
+    if (msg.indexOf('[JASMINE]') == 0) {
+        msg = msg.substring(9);
+        system.stdout.write(msg);
+        var match = doneRegEx.exec(msg);
+        if (match) {
+            rc = match[1] === '0' ? 0 : 1;
+            return;
+        }
+        match = noReallyDoneRegEx.exec(msg);
+        if (match) {
+            system.stdout.writeLine('');
+            phantom.exit(rc);
+        }
     }
-    match = noReallyDoneRegEx.exec(msg);
-    if (match) {
-        system.stdout.writeLine("");
-        phantom.exit(rc);
+    else {
+        console.log('\n\x1B[33m[LOG]\x1B[0m ' + msg);
     }
 };
 
-system.stdout.writeLine("");
+system.stdout.writeLine('');
 page.open(system.args[1], function(status){
-    if (status !== "success") {
-        console.log("Couldn't load the page");
+    if (status !== 'success') {
+        console.log('Could\'t load the page');
     }
-    system.stdout.writeLine("");
+    system.stdout.writeLine('');
 });
 
